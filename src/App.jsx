ad77f7fd047c82bc282e5b50594b2374c934470b
waleed <waleednisar43@gmail.com>
2024-09-26 19:26:52 +0500

    Add Header

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Projects from "./pages/Projects"
 import Signin from "./pages/Signin"
 import Signup from "./pages/Signup"
 import Dashboard from "./pages/Dashboard"
+import Header from "./components/Header"
 
 export default function App(){
   return(
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+const flowbite = require("flowbite-react/tailwind");
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content()
   ],
+  
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    flowbite.plugin()
+  ],
+};
